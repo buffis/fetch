a69e001ad3cab1596bd808cf40eb85fc83be5473
buffis <bjorn.kempen@gmail.com>
2015-03-12 07:50:33 +0100

    Refactors parser into parser + parseractions. Also refactored parsing of output Rexpression

--- fetchparser.py
+++ fetchparser.py
@@ -2,6 +2,7 @@ import ply.yacc as yacc
 
 # Get the token map from the lexer.  This is required.
 from fetchlexer import tokens
+from parseractions import *
 
 def p_fetchsection(p):
     """fetchsection : fetchlines"""
@@ -77,43 +78,32 @@ def p_get(p):
 def p_post(p):
     "post : RARROW"
     p[0] = "POST"
-
+    
 def p_outputline(p):
-    """outputline : outputassign"""
-    p[0] = p[1]
-
-def p_outputassign_list(p):
-    """outputassign : NAME EQUALS outputlist NEWLINE"""
+    """outputline : NAME EQUALS outputright NEWLINE"""
     p[0] = OutputAssignment(p[1], p[3])
 
-def p_outputassign_var(p):
-    """outputassign : NAME EQUALS NAME NEWLINE"""
-    p[0] = OutputAssignment(p[1], p[3])
-
-def p_outputassign_string(p):
-    """outputassign : NAME EQUALS STRING NEWLINE"""
-    p[0] = OutputAssignment(p[1], p[3])
-
-def p_outputassign_arrayitem(p):
-    """outputassign : NAME EQUALS NAME LBRACE NUMBER RBRACE NEWLINE"""
-    p[0] = OutputAssignment(p[1], ListAt(p[3], p[5]))
-
-# TODO: Consider generalizing the right expression
-def p_outputassign_plusexp(p):
-    """outputassign : NAME EQUALS NAME PLUS NAME NEWLINE"""
-    p[0] = OutputAssignment(p[1], ListPlus(p[3], p[5]))
-
-def p_outputassign_dict(p):
-    """outputassign : NAME EQUALS outputdict NEWLINE"""
-    p[0] = OutputAssignment(p[1], p[3])
-
-def p_outputassign_dictkey(p):
-    """outputassign : NAME LBRACE STRING RBRACE EQUALS NAME NEWLINE"""
+def p_outputline_dict(p):
+    """outputline : NAME LBRACE STRING RBRACE EQUALS outputright NEWLINE"""
     p[0] = OutputAssignment(DictAt(p[1], p[3]), p[6])
 
-def p_outputlist(p):
-    """outputlist : LBRACE outputlistitems RBRACE"""
+def p_outputright(p):
+    """outputright : NAME
+                   | STRING
+                   | outputdict"""
+    p[0] = p[1]
+
+def p_outputright_arrayitem(p):
+    """outputright : NAME LBRACE NUMBER RBRACE"""
+    p[0] = ListAt(p[1], p[3])
+
+def p_outputright_list(p):
+    """outputright : LBRACE outputlistitems RBRACE"""
     p[0] = p[2]
+    
+def p_outputright_expression(p):
+    """outputright : NAME PLUS NAME"""
+    p[0] = ListPlus(p[1], p[3])
 
 def p_outputlistitems_single(p):
     """outputlistitems : STRING"""
@@ -147,98 +137,3 @@ def parse_input(i):
     result = parser.parse(i, lexer=lexer)
     return result
 
-class ListPlus(object):
-    def __init__(self, l1, l2):
-        self.l1 = l1
-        self.l2 = l2
-    def __str__(self):
-        return "%s+%s" % (str(self.l1), str(self.l2))
-
-class DictAt(object):
-    def __init__(self, d, at):
-        self.d = d
-        self.at = at
-    def __str__(self):
-        return "%s[%s]" % (str(self.d), str(self.at))
-    
-class ListAt(object):
-    def __init__(self, l, at):
-        self.l = l
-        self.at = at
-    def __str__(self):
-        return "%s[%s]" % (str(self.l), str(self.at))
-
-class OutputAssignment(object):
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-    def __str__(self):
-        return "Assignment: " + str(self.name) + " = " + str(self.value)
-
-class FilterExpression(object):
-    pass
-
-class NegFilterExpression(FilterExpression):
-    def __init__(self, exp):
-        self.exp = exp
-    def __str__(self):
-        return "<!Filter: " + str(self.exp) + ">"
-
-class CombinedFilterExpression(FilterExpression):
-    def __init__(self, exp1, exp2, op):
-        self.exp1 = exp1
-        self.exp2 = exp2
-        self.op = op
-    def __str__(self):
-        return "<Filter: " + str(self.exp1) + str(self.op) + str(self.exp2) + ">"
-
-class BasicFilterExpression(FilterExpression):
-    def __init__(self, key, arg=None):
-        self.key = key
-        self.arg = arg
-    def __str__(self):
-        r = "<Filter:" + self.key
-        if self.arg:
-            r += ":" + self.arg
-        return r + ">"
-
-class FilterAction(object):
-    def __init__(self, name, expression, indata):
-        self.name = name
-        self.expression = expression
-        self.indata = indata
-    def __str__(self):
-        return "Filter: " + str(type(self)) + self.name + str(self.expression) + self.indata
-    def __repr__(self):
-        return str(self)
-
-class CoarseFilterAction(FilterAction):
-    pass
-
-class FineFilterAction(FilterAction):
-    pass
-
-class FetchAction(object):
-    def __init__(self, name, method, url):
-        self.name = name
-        self.method = method
-        self.url = url
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "FetchAction: " + self.name + self.method + self.url
-
-class ModifyUrlAction(object):
-    def __init__(self, name, method, key, value):
-        self.name = name
-        self.method = method
-        self.key = key
-        self.value = value
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "ModifyUrlAction: " + self.name + self.method + self.key + self.value
---
+++ parseractions.py
@@ -0,0 +1,99 @@
+"""
+Actions representing nodes in the parse tree.
+"""
+
+class ListPlus(object):
+    def __init__(self, l1, l2):
+        self.l1 = l1
+        self.l2 = l2
+    def __str__(self):
+        return "%s+%s" % (str(self.l1), str(self.l2))
+
+class DictAt(object):
+    def __init__(self, d, at):
+        self.d = d
+        self.at = at
+    def __str__(self):
+        return "%s[%s]" % (str(self.d), str(self.at))
+    
+class ListAt(object):
+    def __init__(self, l, at):
+        self.l = l
+        self.at = at
+    def __str__(self):
+        return "%s[%s]" % (str(self.l), str(self.at))
+
+class OutputAssignment(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def __str__(self):
+        return "Assignment: " + str(self.name) + " = " + str(self.value)
+
+class FilterExpression(object):
+    pass
+
+class NegFilterExpression(FilterExpression):
+    def __init__(self, exp):
+        self.exp = exp
+    def __str__(self):
+        return "<!Filter: " + str(self.exp) + ">"
+
+class CombinedFilterExpression(FilterExpression):
+    def __init__(self, exp1, exp2, op):
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.op = op
+    def __str__(self):
+        return "<Filter: " + str(self.exp1) + str(self.op) + str(self.exp2) + ">"
+
+class BasicFilterExpression(FilterExpression):
+    def __init__(self, key, arg=None):
+        self.key = key
+        self.arg = arg
+    def __str__(self):
+        r = "<Filter:" + self.key
+        if self.arg:
+            r += ":" + self.arg
+        return r + ">"
+
+class FilterAction(object):
+    def __init__(self, name, expression, indata):
+        self.name = name
+        self.expression = expression
+        self.indata = indata
+    def __str__(self):
+        return "Filter: " + str(type(self)) + self.name + str(self.expression) + self.indata
+    def __repr__(self):
+        return str(self)
+
+class CoarseFilterAction(FilterAction):
+    pass
+
+class FineFilterAction(FilterAction):
+    pass
+
+class FetchAction(object):
+    def __init__(self, name, method, url):
+        self.name = name
+        self.method = method
+        self.url = url
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "FetchAction: " + self.name + self.method + self.url
+
+class ModifyUrlAction(object):
+    def __init__(self, name, method, key, value):
+        self.name = name
+        self.method = method
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "ModifyUrlAction: " + self.name + self.method + self.key + self.value
