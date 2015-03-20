eb84783a851e8698984a94eccde17cb93e926c68
buffis <bjorn.kempen@gmail.com>
2015-03-20 08:12:07 +0100

    Added a bunch of more filters to the interpreter

--- fetchinterpreter.py
+++ fetchinterpreter.py
@@ -43,6 +43,13 @@ class UrlWrapper(object):
             self.do_request()
         return self.text_wrapper.map(exp)
 
+def striptags(x, v):
+    tags = v.split(",")
+    subbed = x
+    for tag in tags:
+        subbed = re.sub(r'<%s.*?>.*?</%s>' % (tag, tag), '', subbed)
+        subbed = re.sub(r'<%s.*?/>' % tag, '', subbed)
+    return subbed
 
 VARS = {}
 
@@ -67,6 +74,19 @@ def modifyurlaction(action):
 
 # TODO: Enforce validation?
 def starts_filter(arg): return lambda x: x.startswith(arg)
+def ends_filter(arg): return lambda x: x.endswith(arg)
+def contains_filter(arg): return lambda x: s in arg
+def matches_filter(arg): return lambda x: re.compile(arg).match(x)
+# TODO: def length_filter(arg): return lambda x: len(x) arg
+
+def after_filter(arg): return lambda x: x[x.find(arg)+len(arg)-2:] if arg in x else ''
+def before_filter(arg): return lambda x: x[:x.find(arg)] if arg in x else x
+def afterpos_filter(arg): return lambda x: x[arg:]
+def beforepos_filter(arg): return lambda x: x[:arg]
+def exclude_filter(arg): return lambda x: x.replace(arg, '')
+def striptags_filter(arg): return lambda x: striptags(x, arg)
+
+def text_filter(arg): return lambda x: x #TODO
 
 def filter_expression(exp, filter_map):
     t = type(exp)
@@ -76,8 +96,25 @@ def filter_expression(exp, filter_map):
 def coarsefilteraction(action):
     coarse_filter_map = {
         "starts" : starts_filter,
+        "ends" : ends_filter,
+        "containts" : contains_filter,
+        "matches" : matches_filter,
+        #"length" : length_filter, TODO
     }
     f = filter_expression(action.expression, coarse_filter_map)
+    VARS[action.name] = VARS[action.indata].filter(f)
+
+def finefilteraction(action):
+    fine_filter_map = {
+        "after" : after_filter,
+        "before" : before_filter,
+        "afterpos" : after_filter,
+        "beforepos" : before_filter,
+        "text" : text_filter,
+        "exclude" : exclude_filter,
+        "striptags" : striptags_filter,
+    }
+    f = filter_expression(action.expression, fine_filter_map)
     VARS[action.name] = VARS[action.indata].filter(f)
 
 ##################### END FILTER SECTION #######################
@@ -87,7 +124,7 @@ def handle_line(line):
         FetchAction : fetchaction,
         ModifyUrlAction : modifyurlaction,
         CoarseFilterAction : coarsefilteraction,
-        FineFilterAction : lambda x: x,
+        FineFilterAction : finefilteraction,
         OutputAssignment : lambda x: x,
     }
     action_map[type(line)](line)
