654a0f003b27b6873c4a51c4babd1a48146ddc95
buffis <bjorn.kempen@gmail.com>
2015-04-26 11:24:33 +0200

    Disallow importing the debug utils

--- fetchdebugutils.py
+++ fetchdebugutils.py
@@ -1,6 +1,12 @@
+"""
+Stuff that can be used to debug Fetch files and/or the fetch lexer, parser or interpreter.
+
+IMPORTANT: Other modules are _not_ allowed to have dependencies on this one.
+"""
+
 def print_lexed(filename):
     import fetchlexer
-    l=fetchlexer.get_lexer()
+    l = fetchlexer.get_lexer()
 
     # Give the lexer some input
     l.input(open(filename).read())
@@ -15,3 +21,8 @@ def print_parsed(filename):
     import fetchparser
     for line in fetchparser.parse_input(open(filename).read()):
         print line
+
+if __name__ == "__main__":
+    pass  # TODO: Implement.
+else:
+    raise RuntimeError("Importing fetchdebugutils from other modules is NOT allowed.")
